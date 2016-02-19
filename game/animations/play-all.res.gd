a67fdd2c25209f25f65c0f1374442a2c4af60ff7
Batiste Bieler <batiste.bieler@local.ch>
2016-02-19 11:09:05 +0100

    Animations

--- game/animations/play-all.res.gd
+++ game/animations/play-all.res.gd
@@ -1,17 +1,26 @@
 extends AnimatedSprite
 
 var tempElapsed = 0
+var nbFrames = 0
+var currentFrame = 0
+export var canBeInterrupted = true
 
 func _ready():
    set_process(true)
+   nbFrames = self.get_sprite_frames().get_frame_count()
    
 func _process(delta):
    tempElapsed = tempElapsed + delta
    
    if(tempElapsed > 0.1):
-      if(get_frame() == self.get_sprite_frames().get_frame_count()-1):
+      if(get_frame() == nbFrames - 1):
          set_frame(0)
       else:
          self.set_frame(get_frame() + 1)
       
       tempElapsed = 0
+
+func canInterrupt():
+	if(get_frame() == 0 or canBeInterrupted):
+		return true
+	return false
--- game/player/player.res.gd
+++ game/player/player.res.gd
@@ -5,26 +5,60 @@ var idleFront
 var walkDown
 
 var animations = {}
+var currentAnimation
 
 func _ready():
-	animations["walk-up"] = get_node("walk-up")
-	animations["walk-down"] = get_node("walk-down")
+	currentAnimation = get_node("idle-front")
+	
+	animations["walk-back"] = get_node("walk-up")
+	animations["walk-front"] = get_node("walk-down")
+	animations["walk-left"] = get_node("walk-left")
+	animations["walk-right"] = get_node("walk-right")
+	
+	animations["attack-left"] = get_node("attack-left")
+	animations["attack-right"] = get_node("attack-right")
+	animations["attack-front"] = get_node("attack-front")
+	animations["attack-back"] = get_node("attack-back")
+	
 	animations["idle-front"] = get_node("idle-front")
+	animations["idle-left"] = get_node("idle-front")
+	animations["idle-right"] = get_node("idle-front")
+	animations["idle-back"] = get_node("idle-front")
+	
+	var node = get_node("walk-up")
+	
 	hideAll()
 	
 func hideAll():
 	for key in animations:
 		animations[key].hide()
 	
-func update_direction(direction):
-	var anim_name = "idle-front"
+func get_anim_direction(direction):
+	if direction.x < 0:
+		return "left"
+	if direction.x > 0:
+		return "right"
 	if direction.y > 0:
-		anim_name = "walk-down"
+		return "front"
 	if direction.y < 0:
-		anim_name = "walk-up"
+		return "back"
+	return "front"
 	
+func update_attack(direction):
+	update('attack-' + get_anim_direction(direction))
+	
+func update_walk(direction):
+	update('walk-' + get_anim_direction(direction))
+
+func update_idle(direction):
+	update('idle-' + get_anim_direction(direction))
+	
+func update(anim_name):
+	if not currentAnimation.canInterrupt():
+		return false
 	hideAll()
 	var anim = animations[anim_name]
+	currentAnimation = anim
 	anim.set_pos(Vector2(-17, -53))
 	anim.show()
 	
